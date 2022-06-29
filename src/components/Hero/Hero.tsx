930eb682831836faf468471a94a57f5443af1d60
Ari Nguyen <quynhnhu3132000@gmail.com>
2022-06-29 00:35:40 -0400

    Add Hero, delete non-necessary stuff

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Row, Col } from "react-flexbox-grid";
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
+import { Button } from '@mui/material';
+
+import Ava from '../../assets/hero/ava.png';
+
+import "./hero.scss";
+
+export default function Hero() {
+  return (
+    <div className="hero" id="hero">
+    <div className="wrapper">
+      <Row>
+        <Col md={12} lg={6}>
+          <div className="hero-info">
+            <h1 className="weight800 font60">
+                Hi! I'm Ari.
+            </h1>
+            <h1 className="weight800 font40">
+              Computer Science Student
+            </h1>
+            <p className="font15">
+              Pre-junior Computer Science Major at Drexel.
+              Interested in the field of AI/ML and Robotics.
+            </p>
+            <Row className="hero-row">
+              <Button variant="contained" href="contact">SEND MESSAGE</Button>
+              <Button variant="contained">DOWNLOAD CV</Button>
+            </Row>
+            <Row className="hero-row">
+              <div id="social-test">
+                <h2>Free Clean Social Buttons</h2>
+                <ul className="social">
+                  <li><FaGithub /></li>
+                  <li><FaLinkedin /></li>
+                </ul>
+              </div>
+            </Row>
+
+          </div>
+        </Col>
+        <Col md={12} lg={6}>
+          <div className="hero-image">
+            <img src={Ava} alt="hero" />
+          </div>
+        </Col>
+      </Row>
+    </div>
+  </div>
+  )
+}
