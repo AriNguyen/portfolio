bde2b24abb378194e0c5a3a92613800c1c59854c
Ari Nguyen <quynhnhu3132000@gmail.com>
2022-07-01 17:48:19 -0400

    Add info in hero

--- src/sections/hero/hero.tsx
+++ src/sections/hero/hero.tsx
@@ -10,43 +10,42 @@ import "./hero.scss";
 export default function Hero() {
   return (
     <div className="hero" id="hero">
-    <div className="wrapper">
-      <Row>
-        <Col md={12} lg={6}>
-          <div className="hero-info">
-            <h1 className="weight800 font60">
+      <div className="wrapper">
+        <Row>
+          <Col md={12} lg={6}>
+            <div className="hero-info">
+              <h1 className="weight800 font60">
                 Hi! I'm Ari.
-            </h1>
-            <h1 className="weight800 font40">
-              Computer Science Student
-            </h1>
-            <p className="font15">
-              Pre-junior Computer Science Major at Drexel.
-              Interested in the field of AI/ML and Robotics.
-            </p>
-            <Row className="hero-row">
-              <Button target='' label="SEND MESSAGE"></Button>
-              <Button target='' label="DOWNLOAD CV"></Button>
-            </Row>
-            <Row className="hero-row">
-              <div id="social-test">
-                <h2>Free Clean Social Buttons</h2>
-                <ul className="social">
-                  <li><FaGithub /></li>
-                  <li><FaLinkedin /></li>
-                </ul>
-              </div>
-            </Row>
-
-          </div>
-        </Col>
-        <Col md={12} lg={6}>
-          <div className="hero-image">
-            <img src={Ava} alt="hero" />
-          </div>
-        </Col>
-      </Row>
+              </h1>
+              <h1 className="weight800 font40">
+                Computer Science Student
+              </h1>
+              <p className="font15">
+                Pre-junior Computer Science Major at Drexel.
+                Interested in the field of AI/ML and Robotics.
+              </p>
+              <Row className="hero-row">
+                <Button target='' label="SEND MESSAGE"></Button>
+                <Button target='' label="DOWNLOAD CV"></Button>
+              </Row>
+              <Row className="hero-row">
+                <div id="social-test">
+                  <h2>Free Clean Social Buttons</h2>
+                  <ul className="social">
+                    <li><FaGithub /></li>
+                    <li><FaLinkedin /></li>
+                  </ul>
+                </div>
+              </Row>
+            </div>
+          </Col>
+          <Col md={12} lg={6}>
+            <div className="hero-image">
+              <img src={Ava} alt="hero" />
+            </div>
+          </Col>
+        </Row>
+      </div>
     </div>
-  </div>
   )
 }
