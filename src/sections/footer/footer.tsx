555ee5fc8799865d65f6c5fddbf148239c6636ba
Ari Nguyen <quynhnhu3132000@gmail.com>
2022-07-01 17:42:37 -0400

    Done Footer

---
+++ src/sections/footer/footer.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Row, Col } from "react-flexbox-grid";
+import { Link } from "react-scroll";
+
+import Arrow from "../../assets/footer/arrow.svg";
+
+import "./footer.scss";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="wrapper">
+        <Row>
+          <Col xs={12} sm={6} md={6}>
+            <div className="footer-box">
+              {/* <img src={Logo} alt="logo" /> */}
+              <p>© 2020 - Mint,All Right Reserved</p>
+            </div>
+          </Col>
+          <Col xs={12} sm={6} md={6}>
+            <Link to="hero" spy={true} smooth={true} offset={0} duration={500}>
+              <div className="footer-box back-to-top">
+                <p>BACK TO TOP</p>
+                <img src={Arrow} alt="arrow" />
+              </div>
+            </Link>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  )
+}
